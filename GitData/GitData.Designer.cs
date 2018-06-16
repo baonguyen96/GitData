b2f9e509f46c99859c1c399bd15a0fbb3ec266c9
baonguyen96 <baonguyendrump@gmail.com>
2018-06-16 09:41:43 -0500

    Initial commit

---
+++ GitData/GitData.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GitData
+{
+    partial class GitData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(74, 41);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(538, 20);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.Text = "Enter GitHub account URL here";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(618, 41);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // GitData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 652);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "GitData";
+            this.Text = "GitData";
+            this.Load += new System.EventHandler(this.GitData_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ GitData/GitData.cs
@@ -0,0 +1,31 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitData
+{
+    public partial class GitData : Form
+    {
+        public GitData()
+        {
+            InitializeComponent();
+        }
+
+        private void GitData_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
